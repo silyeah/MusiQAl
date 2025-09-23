67d3ae66df40d5230799bbf899290bdbfd3fcfac
ec-sarapje <ec-sarapje@login-1.fox.ad.fp.educloud.no>
2025-09-23 16:28:29 +0200

    updated bash

--- AVST/Test_sara.sh
+++ AVST/Test_sara.sh
@@ -2,27 +2,30 @@
 
 
 #SBATCH --job-name=trial0
-#SBATCH --account=ec54 
-#SBATCH --time=00:05:00
+#SBATCH --account=ec54
+#SBATCH --time=00:02:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 # #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=1G
+##SBATCH --mem-per-cpu=1G
 #SBATCH --partition=accel  
 #SBATCH --gres=gpu:1
+#SBATCH --mem=8G      
+
 ##SBATCH --qos=devel
-# #SBATCH --mem=32G      
 # #SBATCH --gpus=a100:1
 # #SBATCH --gpus=rtx30:1
 
-# #SBATCH --output=./slurm_outs/test_avst_out_%j.txt
 
-#SBATCH --output=./test_avst_out.txt
+
+## SBATCH --output=./slurm_outs/test_avst_out_%j.txt
+
+#SBATCH --output=./slurm_test_avst_out.txt
 
 module purge
 module load UCX-CUDA/1.12.1-GCCcore-11.3.0-CUDA-11.7.0
-module load Python/3.9.6-GCCcore-11.2.0
-source /fp/homes01/u01/ec-sarapje/MusiQAl/compat_env/bin/activate
+# # module load Python/3.9.6-GCCcore-11.2.0 
+source /fp/homes01/u01/ec-sarapje/MusiQAl/new_env/bin/activate
 
 
 # # source /your/env/dir/avqaoriginal/bin/activate
@@ -34,7 +37,13 @@ cd $DIR &> /dev/null
 # #pwd > /fp/homes01/u01/ec-sarapje/MusiQAl/AVST/pwd.txt
 # #python main_avst.py > ./output_test.txt
 
-python main_avst.py > ./avst_test.txt
+
+python avst_test_stripped.py > ./avst_test_stripped.txt
+
+#python main_avst.py > ./avst_test.txt
+
+
+
 
 
 
