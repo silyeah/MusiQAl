92627bac70a149c902535a02445ae084ffeebe1f
ec-sarapje <ec-sarapje@login-4.fox.ad.fp.educloud.no>
2025-09-30 11:14:26 +0200

    select success samples

---
+++ AVST/json/clean_json.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import json
+import os
+
+failed_filename = '../net_grd_avst/test_results/failed_questions_run_0.csv'
+
+df = pd.read_csv(failed_filename)
+failed_idx = df['idx'].tolist()
+
+org_json = json.load(open('avqa-test.json', 'r'))
+
+print(len(org_json))
+
+
+for idx in reversed(failed_idx):
+    org_json.pop(idx)
+
+print(len(org_json))
+
+
+with open('avqa-test-success.json', 'w') as f:
+    json.dump(org_json, f, indent = 4)
+
+print('Cleaned json saved to avqa-test-success.json')
+
+check_json = json.load(open('avqa-test-success.json', 'r'))
+print(len(check_json))
+
+print(check_json[0])
